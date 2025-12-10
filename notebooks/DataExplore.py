da1a464710a21d58da8668940d00f3f81f426554
Samhitha <asjasti07@gmail.com>
2025-12-10 20:11:58 +0530

    Updated requirements.txt and data explore

---
+++ notebooks/DataExplore.py
@@ -0,0 +1,5 @@
+import pandas as pd
+data=pd.read_csv(r'C:\Users\asjas\OneDrive\Documents\Docker projects\churn-retention-forecasting\data\raw\customer_churn_dataset-training-master.csv')
+data
+
+data.shape()
