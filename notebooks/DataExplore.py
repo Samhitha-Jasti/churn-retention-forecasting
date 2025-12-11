164bd2d41647de498438375aae6ac7210480dd24
Samhitha <asjasti07@gmail.com>
2025-12-11 22:34:06 +0530

    Explored data a little

--- notebooks/DataExplore.py
+++ notebooks/DataExplore.py
@@ -2,4 +2,16 @@ import pandas as pd
 data=pd.read_csv(r'C:\Users\asjas\OneDrive\Documents\Docker projects\churn-retention-forecasting\data\raw\customer_churn_dataset-training-master.csv')
 data
 
-data.shape()
+data.shape
+
+data.head()
+
+data.isnull().sum()
+
+data[data.isnull().any(axis=1)]
+
+'''
+Problems Detected:
+- Customer ID starts from 2 instead of 1
+- ID is in format 'x.0' instead of 'x'
+'''
